183b2b344f9f02b26d40071f900aa84bddfd76cc
Jia Huang <iamfat@gmail.com>
2023-08-08 11:56:15 +0800

    fix: esbuild for both cjs/mjs

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,9 +1,22 @@
 import { build } from 'esbuild';
+
 build({
     entryPoints: ['src/index.browser.ts'],
     platform: 'browser',
     bundle: true,
     format: 'esm',
+    outfile: 'dist/index.browser.mjs',
+    define: {
+        'process.env.NODE_ENV': '"production"',
+    },
+    external: ['@genee/fetch'],
+});
+
+build({
+    entryPoints: ['src/index.browser.ts'],
+    platform: 'browser',
+    bundle: true,
+    format: 'cjs',
     outfile: 'dist/index.browser.js',
     define: {
         'process.env.NODE_ENV': '"production"',
@@ -16,6 +29,18 @@ build({
     platform: 'node',
     bundle: true,
     format: 'esm',
+    outfile: 'dist/index.mjs',
+    define: {
+        'process.env.NODE_ENV': '"production"',
+    },
+    external: ['@genee/fetch'],
+});
+
+build({
+    entryPoints: ['src/index.ts'],
+    platform: 'node',
+    bundle: true,
+    format: 'cjs',
     outfile: 'dist/index.js',
     define: {
         'process.env.NODE_ENV': '"production"',
